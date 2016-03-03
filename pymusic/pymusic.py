38ae52feb88d406a3c59d7a6ed346a7b158a1db4
Jordan Hand <jordanhand@comcast.net>
2016-03-02 16:08:20 -0800

    breaking changes

--- pymusic/pymusic.py
+++ pymusic/pymusic.py
@@ -7,18 +7,60 @@ from curses import wrapper
 import crawl
 
 
-def main(scr):
-    """Does curses stuffs"""
+class MusicUI(object):
 
-    scr.addstr(0, 0, "PYMusic Music Player")
-    songs = crawl.find_files("/home/jordan/Music")
-    index = 2
-    for s in songs:
-        scr.addstr(index, 0, s.name)
-        index += 1
+    def __init__(self, screen):
+        self.scr = screen
 
-    scr.refresh()
-    scr.getch()
+    def refresh(self, nrows, xmax):
+        self.scr.refresh()
+        self.pad.refresh(pad_pos, 0, 1, 0, nrows, xmax - 1)
+
+    def init_ui(self, songs, refresh_only):
+
+        self.scr.addstr(0, 0, "PYMusic Music Player")
+
+        self.pad = curses.newpad(100, 100)
+        index = 1
+        self.pad_pos = 1
+
+        for s in songs:
+            if index == pad_pos:
+                mode = curses.A_REVERSE
+            else:
+                mode = curses.A_NORMAL
+                pad.addstr(index, 2, s.name, mode)
+                index += 1
+        max_x = self.scr.getmaxyx()
+        curses.curs_set(1)
+        curses.curs_set(0)
+        refresh(len(songs), max_x, pad)
+
+    def add_pad()
+
+    def get_input(songs):
+        while True:
+            ch = scr.getch()
+            if ch == curses.KEY_DOWN:
+                if pad_pos < len(songs):
+                    pad_pos += 1
+            elif ch == curses.KEY_UP:
+                if pad_pos > 0:
+                    pad_pos -= 1
+            elif ch == ord('q'):
+                break
+            refresh(len(songs))
 
 
-wrapper(main)
+class App(object):
+    def __init__(self, screen):
+        self.screen = screen
+
+        songs = crawl.find_files("/home/jordan/Music")
+        ui = MusicUI(screen)
+        draw_ui(songs)
+        get_input(songs)
+
+
+if __name__ == "__main__":
+    wrapper(App)
